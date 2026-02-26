c26b5f44282ad067037e3f57335831bf91c1871b
ap0phasi <66261738+ap0phasi@users.noreply.github.com>
2026-02-26 20:44:06 +1100

    BTreeMap and IndexSet idea

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
+use indexmap::IndexSet;
 use markdown;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
-#[derive(Debug, Hash, Eq, PartialEq)]
-struct Link {
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+struct StructEdge {
     from: String,
     to: String,
     filename: String,
 }
 
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+struct Test {
+    a: String,
+    b: String,
+}
+
 fn insert_into_table(
     md_base: &str,
-    mut edge_table: HashMap<Link, i32>,
+    mut edge_table: BTreeMap<StructEdge, i32>,
+    filename: &str,
     id: i32,
-) -> HashMap<Link, i32> {
+) -> BTreeMap<StructEdge, i32> {
     let md_result = markdown::to_html(md_base);
-    let mut md_vec = md_result.split("\n").collect::<Vec<&str>>();
-    md_vec.insert(0, "<ORIGIN>");
-    md_vec.append(&mut vec!["<END>"]);
+    let mut md_vec = md_result
+        .split("\n")
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    md_vec.insert(0, format!("<ORIGIN{:?}>", filename));
+    md_vec.append(&mut vec![format!("<END{:?}>", filename)]);
 
     for i in 0..md_vec.len() - 1 {
         edge_table
-            .entry(Link {
+            .entry(StructEdge {
                 from: md_vec[i].to_string(),
                 to: md_vec[i + 1].to_string(),
-                filename: "hello.md".to_string(),
+                filename: filename.to_string(),
             })
             .or_insert(id);
     }
@@ -34,10 +45,28 @@ fn insert_into_table(
 fn main() {
     let md_base: &str = "# Hi, *Saturn*! 🪐\nThis is some text\n## Another header\nMore text\n## New Header\n More text";
     // Hashmap tests
-    let mut edge_table: HashMap<Link, i32> = HashMap::new();
-    edge_table = insert_into_table(md_base, edge_table, 0);
+    let mut edge_table: BTreeMap<StructEdge, i32> = BTreeMap::new();
+    edge_table = insert_into_table(md_base, edge_table, "test_file.md", 0);
+    let md_new: &str = "# Hi, *Saturn*! 🪐\nThis is some text\n## Another header\nMore text\n## New Header\n More text";
+    edge_table = insert_into_table(md_new, edge_table, "test_file.md", 1);
     println!("{:?}", edge_table);
-    let md_new: &str = "# Hi, *Saturn*! 🪐\nThis is some text\nThis is some more text that is new\n## Another header\nMore text\n## New Header\n More text ## A Newer Header\nBlah Blah";
-    edge_table = insert_into_table(md_new, edge_table, 1);
-    println!("{:?}", edge_table);
+
+    // Trace Test
+    let mut set = IndexSet::new();
+    set.insert(Test {
+        a: "Apple".to_string(),
+        b: "hi".to_string(),
+    });
+    set.insert(Test {
+        a: "Banana".to_string(),
+        b: "hi".to_string(),
+    });
+
+    println!(
+        "{:?}",
+        set.get_index_of(&Test {
+            a: "Apple".to_string(),
+            b: "hi".to_string()
+        })
+    )
 }
