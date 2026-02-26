f39d786229e058c7edecb2d87b9fef0ddf2dee78
ap0phasi <66261738+ap0phasi@users.noreply.github.com>
2026-02-26 22:31:20 +1100

    Merge and reconstruction from graph

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,21 @@ struct Node {
     filename: String,
 }
 
+#[derive(Debug)]
+struct Edge {
+    num: i32,
+    tag: String,
+}
+
 fn insert_into_table<'a>(
     md_base: &str,
-    mut edge_table: BTreeMap<(usize, usize), &'a str>,
+    mut edge_table: BTreeMap<(usize, usize), Edge>,
     mut node_table: IndexSet<Node>,
     filename: &str,
     id: i32,
-) -> (
-    BTreeMap<(usize, usize), &'a str>,
-    IndexSet<Node, RandomState>,
-) {
+) -> (BTreeMap<(usize, usize), Edge>, IndexSet<Node, RandomState>) {
     let md_result = markdown::to_html(md_base);
-    let mut md_vec = md_result
+    let md_vec = md_result
         .split("\n")
         .map(|s| Node {
             content: s.to_string(),
@@ -36,25 +39,43 @@ fn insert_into_table<'a>(
                 node_table.get_index_of(&md_vec[i - 1]).unwrap(),
                 node_table.get_index_of(&md_vec[i]).unwrap(),
             ))
-            .or_insert("hi");
+            .or_insert(Edge {
+                num: id,
+                tag: "hello".to_string(),
+            });
     }
     (edge_table, node_table)
 }
 
 fn main() {
-    let md_base: &str = "# Hi, *Saturn*! 🪐\nThis is some text\n## Another header\nMore text\n## New Header\n More texts";
     // Hashmap tests
-    let mut edge_table: BTreeMap<(usize, usize), &str> = BTreeMap::new();
+    let mut edge_table: BTreeMap<(usize, usize), Edge> = BTreeMap::new();
     let mut node_table = IndexSet::new();
+
+    let md_base: &str = "# Hi, *Saturn*! 🪐\nThis is some text\n## Another header\nMore text\n## New Header\n More texts";
     (edge_table, node_table) = insert_into_table(md_base, edge_table, node_table, "test.md", 0);
+    let md_base: &str = "# Hi, *Saturn*! 🪐\nThis is some text\n## Another header\nMore text\n## News Header\n More texts";
+    (edge_table, node_table) = insert_into_table(md_base, edge_table, node_table, "test.md", 1);
+    let md_base: &str = "# Hi, *Saturn*! 🪐\nThis is some new text\n## Another header\nMore text\n## New Header\n More texts";
+    (edge_table, node_table) = insert_into_table(md_base, edge_table, node_table, "test.md", 2);
 
     println!("{:?}", node_table);
     println!("{:?}", edge_table);
 
-    // Query BTreeMap
-    for (k, v) in edge_table.range((1, usize::MIN)..(3, usize::MIN)) {
-        println!("{:?}", (k, v))
-    }
+    println!("{:?}", node_table.get_index(0));
 
-    println!("{:?}", node_table.get_index(4))
+    let mut use_idx = 0;
+    let mut proceed = true;
+    while proceed {
+        proceed = false;
+        let mut max_value = -1000;
+        for (k, v) in edge_table.range((use_idx, usize::MIN)..(use_idx + 1, usize::MIN)) {
+            proceed = true;
+            if v.num > max_value {
+                max_value = v.num;
+                use_idx = k.1;
+            }
+        }
+        println!("{:?}", node_table.get_index(use_idx));
+    }
 }
